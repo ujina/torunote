e9aa1d23fbbfe7e7f8f463ed5e6daf355e89961e
Toru Ogasawara <ujina@me.com>
2022-07-31 15:20:37 +0900

    footer 追加

--- docs/blog/.vuepress/config.js
+++ docs/blog/.vuepress/config.js
@@ -3,6 +3,27 @@ module.exports = {
     title: 'TORU NOTE ', // Title for the site. This will be displayed in the navbar.
     theme: '@vuepress/theme-blog',
     themeConfig: {
+      footer: {
+        copyright: [
+          {
+            text: 'Privacy Policy',
+            link: 'https://policies.google.com/privacy?hl=en-US',
+          },
+          {
+            text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          },
+        ],
+        contact: [
+          {
+            type: 'github',
+            link: 'https://github.com/ujina',
+          },
+          {
+            type: 'twitter',
+            link: 'https://twitter.com/ogasawaratoru',
+          },
+        ],
+      },
       // Please keep looking down to see the available options.
     plugins: [
         '@vuepress/blog',
